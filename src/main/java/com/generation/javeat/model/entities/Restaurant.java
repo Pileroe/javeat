f379d57fa6088e7a883dad1420624b9b53f0ef69
Alessandro <irelip.a@gmail.com>
2024-02-27 16:07:46 +0100

    listString

--- src/main/java/com/generation/javeat/model/entities/Restaurant.java
+++ src/main/java/com/generation/javeat/model/entities/Restaurant.java
@@ -1,11 +1,15 @@
 package com.generation.javeat.model.entities;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -34,6 +38,11 @@ public class Restaurant {
     private String phone, imgUrl;
     private Integer openingHour, closingHour, positionX, positionY, maxDeliveryDistance, name;
     private double deliveryPricePerUnit;
+    
+
+    @ElementCollection(targetClass = String.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "foodType", joinColumns = @JoinColumn(name = "restauran_id"))
+    @Column(name = "foodTypes", nullable = false)
     private List<String> foodTypes;
 
     @JsonIgnore
