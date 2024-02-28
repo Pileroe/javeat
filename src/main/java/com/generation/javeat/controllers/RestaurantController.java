1a6733b07a80e923862fa37fe4bfa6b6ede8990f
Alessia Casamatta <alessiacasamatta@gmail.com>
2024-02-28 14:31:41 +0100

    versione13

--- src/main/java/com/generation/javeat/controllers/RestaurantController.java
+++ src/main/java/com/generation/javeat/controllers/RestaurantController.java
@@ -1,19 +1,22 @@
 package com.generation.javeat.controllers;
+import static com.generation.javeat.utils.Utils.calculateDistanceToRestaurant;
+
 import java.util.Collections;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.generation.javeat.model.dto.restaurant.FilteredRestaurantRqst;
 import com.generation.javeat.model.dto.restaurant.RestaurantDtoWMenu;
-import com.generation.javeat.model.dto.restaurant.RestaurantDtoWNoDellivery;
+import com.generation.javeat.model.dto.restaurant.RestaurantDtoWNoDelivery;
 import com.generation.javeat.model.dtoservices.RestaurantConverter;
 import com.generation.javeat.model.entities.Restaurant;
 import com.generation.javeat.model.repositories.RestaurantRepository;
-import static  com.generation.javeat.utils.Utils.*;
 
 
 
@@ -28,7 +31,7 @@ public class RestaurantController {
     RestaurantConverter RConv;
 
     @GetMapping("/allrestaurants")
-    public List<RestaurantDtoWNoDellivery> allrestaurants() {
+    public List<RestaurantDtoWNoDelivery> allrestaurants() {
 
         return rRepo.findAll().stream().map(e -> RConv.restaurantDtoWNoDellivery(e)).toList();
     }
@@ -39,7 +42,7 @@ public class RestaurantController {
     }
 
     @PostMapping("/restaurants")
-    public List<RestaurantDtoWNoDellivery> restaurant(@RequestBody FilteredRestaurantRqst dto) {
+    public List<RestaurantDtoWNoDelivery> restaurant(@RequestBody FilteredRestaurantRqst dto) {
         List<Restaurant> filtratiDistanza = rRepo.findAll().stream()
                 .filter(f -> calculateDistanceToRestaurant(f,dto.getPositionX(),dto.getPositionY())<dto.getDistance()).toList();
 
