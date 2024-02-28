09be2373f38390bd9f9350e6667bbae264594c4a
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2024-02-28 11:54:30 +0100

    v3

--- src/main/java/com/generation/javeat/controllers/RestaurantController.java
+++ src/main/java/com/generation/javeat/controllers/RestaurantController.java
@@ -1,26 +1,17 @@
 package com.generation.javeat.controllers;
-
 import java.util.Collections;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.generation.javeat.model.dto.login.LoginRequest;
 import com.generation.javeat.model.dto.restaurant.FilteredRestaurantRqst;
 import com.generation.javeat.model.dto.restaurant.RestaurantDtoWNoDellivery;
-import com.generation.javeat.model.dto.user.UserDtoWWithID;
 import com.generation.javeat.model.dtoservices.RestaurantConverter;
-import com.generation.javeat.model.dtoservices.UserConverter;
 import com.generation.javeat.model.entities.Restaurant;
-import com.generation.javeat.model.entities.User;
 import com.generation.javeat.model.repositories.RestaurantRepository;
-import com.generation.javeat.model.repositories.UserRepository;
+
 
 @RestController
 public class RestaurantController {
