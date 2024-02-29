022ecc3dc422e4e477e0554b7e0c45a5370b3474
Alessia Casamatta <alessiacasamatta@gmail.com>
2024-02-29 18:00:58 +0100

    versione21

--- src/main/java/com/generation/javeat/controllers/DeliveryController.java
+++ src/main/java/com/generation/javeat/controllers/DeliveryController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,6 +47,21 @@ public class DeliveryController
        
         return ResponseEntity.ok(repo.save(op));
         
+    }
+
+    @DeleteMapping("/deliveries/{id}")
+    public ResponseEntity<?> delete(@PathVariable Integer id)
+    {
+        Optional<Delivery> op= repo.findById(id);
+        if(op.isPresent())
+        {
+            repo.deleteById(id);
+            return new ResponseEntity<String>("", HttpStatus.OK);
+        }
+        else 
+            return new ResponseEntity<String>("No delivery with id"+id, HttpStatus.NOT_FOUND);
+
+
     }
     
 }
--- src/main/java/com/generation/javeat/model/dto/restaurant/DtoMenuDishes.java
+++ src/main/java/com/generation/javeat/model/dto/restaurant/DtoMenuDishes.java
@@ -1,5 +1,15 @@
 package com.generation.javeat.model.dto.restaurant;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+
 public class DtoMenuDishes 
 {
     private Integer menu_id; 
